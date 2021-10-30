bc3962e3f0ccc2d1683cb5a285999a2077438eb4
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-10-30 10:54:17 -0300

    feat: add login and list message components

---
+++ web/src/components/MessageList/index.tsx
@@ -0,0 +1,56 @@
+import styles from "./styles.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
+export function MessageList() {
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser massa!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/diegogasparcruz.png"
+                alt="Diego Gaspar"
+              />
+            </div>
+            <span>Diego Gaspar</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser massa!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/diegogasparcruz.png"
+                alt="Diego Gaspar"
+              />
+            </div>
+            <span>Diego Gaspar</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser massa!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/diegogasparcruz.png"
+                alt="Diego Gaspar"
+              />
+            </div>
+            <span>Diego Gaspar</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
