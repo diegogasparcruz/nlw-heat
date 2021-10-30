270b173dada48b39f572fd4f0224354ef43ca7aa
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-10-30 02:11:59 -0300

    feat: add create message

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthnticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthnticated,
+  new CreateMessageController().handle
+);
 
 export { router };
