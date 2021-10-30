e67befc8283f275dbbcd9726e928922f6b0b186f
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-10-30 00:38:43 -0300

    feat: add login and callback routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,19 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
+
+app.get("/github", (request, respose) => {
+  respose.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, respose) => {
+  const { code } = request.query;
+
+  return respose.json(code);
+});
 
 const PORT = 4000;
 
